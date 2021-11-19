288414777b1a1012d10fdee9a8fba6cf118ccc34
subhadipTester <37734353+subhadipTester@users.noreply.github.com>
2021-11-19 18:52:20 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+# This Phython program will print Hellow World...
+print("Hello World Demo...\n")
+print("Hello World Demo...\n")
+print("Hello World Demo...\n")
+print("Hello World Demo...\n")
+print("Hello World Demo...\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
